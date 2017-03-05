4fcbdef28d67dc9910406f5286fe28b077470270
darwindeveloper <dsmr.apps@gmail.com>
2017-03-05 00:30:59 -0500

    gradle para publicar en jitpack2

--- onecalendar/src/main/java/com/darwindeveloper/onecalendar/views/OneCalendarView.java
+++ onecalendar/src/main/java/com/darwindeveloper/onecalendar/views/OneCalendarView.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
@@ -20,7 +18,6 @@ import android.widget.TextView;
 
 import com.darwindeveloper.onecalendar.R;
 import com.darwindeveloper.onecalendar.clases.Day;
-import com.darwindeveloper.onecalendar.clases.OnSwipeTouchListener;
 import com.darwindeveloper.onecalendar.fragments.MonthFragment;
 
 import java.text.SimpleDateFormat;
